e949672604b03802271c947369aaa5d0d6937321
Raf <RafhaanShah@users.noreply.github.com>
2020-08-04 10:37:22 +0000

    Make JUnit imports consistent

--- dst/arraylist/ArrayListTest.java
+++ dst/arraylist/ArrayListTest.java
@@ -5,14 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ArrayListTest {
 
     private ArrayList<Object> arrayList;
 
-    @Before
+    @BeforeEach
     public void setup() {
         arrayList = new ArrayList<Object>();
     }
--- dst/set/SetTest.java
+++ dst/set/SetTest.java
@@ -4,14 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SetTest {
 
     private Set<Object> set;
 
-    @Before
+    @BeforeEach
     public void setup() {
         set = new Set<Object>();
     }
