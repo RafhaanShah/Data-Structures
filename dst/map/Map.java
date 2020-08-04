27e59aac22e1032533761f5babd7ccdf681313d7
Raf <RafhaanShah@users.noreply.github.com>
2020-08-04 12:47:56 +0000

    Add Map

---
+++ dst/map/Map.java
@@ -0,0 +1,128 @@
+package dst.map;
+
+public class Map<K, V> {
+
+    public int size = 0;
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    @SuppressWarnings("unchecked")
+    private MapEntry<K, V>[] data = new MapEntry[DEFAULT_CAPACITY];
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+
+        size = 0;
+    }
+
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+
+    public boolean containsValue(V value) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].value.equals(value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].key.equals(key)) {
+                return data[i].value;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public K[] keys() {
+        K[] keys = (K[]) new Object[size];
+
+        for (int i = 0; i < size; i++) {
+            keys[i] = data[i].key;
+        }
+
+        return keys;
+    }
+
+    public V put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].key.equals(key)) {
+                data[i].value = value;
+                return value;
+            }
+        }
+
+        ensureCapacity();
+        data[size] = new MapEntry<K, V>(key, value);
+        size++;
+        return value;
+    }
+
+    public V remove(K key) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].key.equals(key)) {
+                V value = data[i].value;
+                shrinkMap(i);
+                return value;
+            }
+        }
+
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public V[] values() {
+        V[] values = (V[]) new Object[size];
+
+        for (int i = 0; i < size; i++) {
+            values[i] = data[i].value;
+        }
+
+        return values;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void ensureCapacity() {
+        if (data.length == size) {
+            MapEntry<K, V>[] temp = new MapEntry[DEFAULT_CAPACITY];
+            for (int i = 0; i < size; i++) {
+                temp[i] = data[i];
+            }
+            data = temp;
+        }
+    }
+
+    private void shrinkMap(int index) {
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+
+        data[size] = null;
+        size--;
+    }
+
+    private class MapEntry<A, B> {
+
+        private final A key;
+        private B value;
+
+        public MapEntry(A k, B v) {
+            key = k;
+            value = v;
+        }
+
+    }
+
+}
---
+++ dst/map/MapTest.java
@@ -0,0 +1,163 @@
+package dst.map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MapTest {
+
+    private Map<Object, Object> map;
+
+    @BeforeEach
+    public void setup() {
+        map = new Map<Object, Object>();
+    }
+
+    @Test
+    public void isEmpty_onEmptyMap() {
+        assertTrue(map.isEmpty());
+        assertEquals(0, map.size);
+    }
+
+    @Test
+    public void put_onEmptyMap() {
+        Object key = new Object();
+        Object value = new Object();
+
+        assertEquals(value, map.put(key, value));
+        assertFalse(map.isEmpty());
+        assertEquals(1, map.size);
+    }
+
+    @Test
+    public void put_onExistingKey() {
+        Object key = new Object();
+        Object value = new Object();
+        Object newValue = new Object();
+
+        map.put(key, value);
+        map.put(key, newValue);
+
+        assertEquals(newValue, map.get(key));
+    }
+
+    @Test
+    public void get_onEmptyMap() {
+        assertNull(map.get(new Object()));
+    }
+
+    @Test
+    public void get_onMap() {
+        Object key = new Object();
+        Object value = new Object();
+
+        assertEquals(value, map.put(key, value));
+        assertFalse(map.isEmpty());
+        assertEquals(1, map.size);
+    }
+
+    @Test
+    public void remove_onEmptyMap() {
+        assertNull(map.remove(new Object()));
+    }
+
+    @Test
+    public void remove_onMap() {
+        Object key = new Object();
+        Object value = new Object();
+
+        map.put(key, value);
+
+        assertEquals(value, map.remove(key));
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void containsKey_onEmptyMap() {
+        assertFalse(map.containsKey(new Object()));
+    }
+
+    @Test
+    public void containsKey_onMap() {
+        Object key = new Object();
+        Object value = new Object();
+        map.put(key, value);
+
+        assertTrue(map.containsKey(key));
+    }
+
+    @Test
+    public void containsValue_onEmptyMap() {
+        assertFalse(map.containsValue(new Object()));
+    }
+
+    @Test
+    public void containsValue_onMap() {
+        Object key = new Object();
+        Object value = new Object();
+        map.put(key, value);
+
+        assertTrue(map.containsValue(value));
+    }
+
+    @Test
+    public void keys_onEmptyMap() {
+        assertEquals(0, map.keys().length);
+    }
+
+    @Test
+    public void keys_containsAllKeys() {
+        Object key = new Object();
+        Object value = new Object();
+        map.put(key, value);
+        map.put(new Object(), new Object());
+        map.put(new Object(), new Object());
+
+        Object[] keys = map.keys();
+        assertEquals(3, keys.length);
+        assertTrue(contains(key, keys));
+    }
+
+    @Test
+    public void values_onEmptyMap() {
+        assertEquals(0, map.values().length);
+    }
+
+    @Test
+    public void values_containsAllValues() {
+        Object key = new Object();
+        Object value = new Object();
+        map.put(key, value);
+        map.put(new Object(), new Object());
+        map.put(new Object(), new Object());
+
+        Object[] values = map.values();
+        assertEquals(3, values.length);
+        assertTrue(contains(value, values));
+    }
+
+    @Test
+    public void clear_emptiesList() {
+        map.put(new Object(), new Object());
+        map.put(new Object(), new Object());
+        map.put(new Object(), new Object());
+
+        map.clear();
+        assertTrue(map.isEmpty());
+    }
+
+    private boolean contains(Object element, Object[] elements) {
+        for (Object o : elements) {
+            if (element.equals(o)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
